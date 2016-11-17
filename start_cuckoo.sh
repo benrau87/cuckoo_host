8cb91976a0f07e37a9ffc3d9b3f113025d80ab2c
benrau87 <benrau87@users.noreply.github.com>
2016-11-17 09:40:53 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+### BEGIN INIT INFO
+# Provides:          Cuckoo Stuffs
+# Required-Start:    $local_fs $network
+# Required-Stop:     
+# Default-Start:     100
+# Default-Stop:      
+# Short-Description: Cuckoo forwarding for client
+# Description:       
+### END INIT INFO
 vboxmanage hostonlyif create
 iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
