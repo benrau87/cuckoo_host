0e677e9982bc93e7d369d1d57cd26f268a4b1e9c
benrau87 <benrau87@users.noreply.github.com>
2016-11-17 09:14:06 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+vboxmanage hostonlyif create
+iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A POSTROUTING -t nat -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
