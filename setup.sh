9a476ab7f15e10a769a15f98271e9a0465a55cb2
benrau87 <benrau87@users.noreply.github.com>
2016-11-21 11:26:45 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,12 +26,12 @@ echo -e "${RED}Installing Dependencies...Please Wait${NC}"
 apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get -qq update -y
-apt-get -qq dist-upgrade -y
 apt-get -qq install mongodb wireshark tcpdump virtualbox python python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev -y
 #apt-get -qq install python python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev mongodb virtualbox tcpdump wireshark -y
 #apt-get install mongodb libffi-dev build-essential python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet tcpdump wireshark virtualbox -y
 #apt-get -qq install python python-pip python-dev libcap2-bin libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev tcpdump mongodb virtualbox -y
 #apt-get -qq install mongodb libffi-dev build-essential python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet virtualbox tcpdump -y
+apt-get -qq dist-upgrade -y
 pip install --upgrade pip
 systemctl enable mongodb.service
 ##tcpdump permissions
