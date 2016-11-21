6731ce1c529d01b46443f9931564c8db39c3730f
benrau87 <benrau87@users.noreply.github.com>
2016-11-21 10:46:38 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-vboxmanage hostonlyif create
+VBoxManage hostonlyif create
+VBoxManage startvm --type headless Win7
 iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A POSTROUTING -t nat -j MASQUERADE
