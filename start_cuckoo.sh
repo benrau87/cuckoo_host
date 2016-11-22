e36a75f17604d1b094664b0c6e7c4efff25c308d
benrau87 <benrau87@users.noreply.github.com>
2016-11-22 08:39:11 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-vboxmanage hostonlyif create
 VBoxManage startvm --type headless Win7
-iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A POSTROUTING -t nat -j MASQUERADE
-sysctl -w net.ipv4.ip_forward=1
+./cuckoo.py
+./web/manage.py runserver 0.0.0.0:8000
