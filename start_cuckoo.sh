6decfbc196b65d6bb93a075919c1fc6150839119
benrau87 <benrau87@users.noreply.github.com>
2016-11-22 12:28:26 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VBoxManage startvm --type headless Win7
-cd web/
+VBoxManage startvm --type headless Win7 Clone
+cd cuckoo/web/
 ./manage.py runserver 0.0.0.0:8000 &
 cd ..
 ./cuckoo.py
