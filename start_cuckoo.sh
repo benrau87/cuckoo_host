162a620e38e6cd20135c56e888adc35ef13cdcd7
benrau87 <benrau87@users.noreply.github.com>
2016-11-23 10:58:19 -0600

    Update start_cuckoo.sh

--- start_cuckoo.sh
+++ start_cuckoo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ON=$(ifconfig | grep -cs 'vboxnet')
+
+if [[ $ON == 1 ]]
+then
+  echo "Host only interface is up"
+else 
+  vboxmange hostonlyif create
+fi
+
 VBoxManage startvm --type headless 'Win7 Clone'
 cd cuckoo/web/
 ./manage.py runserver 0.0.0.0:8000 &
