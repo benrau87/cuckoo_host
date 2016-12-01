58831d90e9aa62c7db32ddaf1b9614c2d58db091
benrau87 <benrau87@users.noreply.github.com>
2016-12-01 11:22:39 -0600

    Create vmcloak.sh

---
+++ vmcloak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo mkdir -p ~/Desktop/windows_ISOs
+##VMCloak
+echo
+read -p "Please place your Windows ISO in the folder on your deskotp and enter Y to continue" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+pip install vmcloak --upgrade
+mount -o loop,ro *.iso ~/Desktop/windows_ISOs 
+vmcloak-vboxnet0
+vmcloak -r --win7x32 win7vm
+fi
