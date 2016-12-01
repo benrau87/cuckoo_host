9c9a457f825b4a6d1a2354850cc03c2f4dc53848
benrau87 <benrau87@users.noreply.github.com>
2016-12-01 14:29:02 -0600

    Update vmcloak.sh

--- vmcloak.sh
+++ vmcloak.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 apt-get install mkisofs genisoimage -y
 sudo mkdir -p /mnt/windows_ISOs
 ##VMCloak
 echo
 read -p "Please place your Windows ISO in the folder under /mnt/windows_ISOs and enter Y to continue" -n 1 -r
 echo
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 pip install vmcloak --upgrade
 mount -o loop,ro  --source /mnt/windows_ISOs/*.iso --target /mnt/windows_ISOs/
 vmcloak-vboxnet0
+echo -e "${YELLOW}This process will take some time, you should get a sandwich...${NC}"
 vmcloak init --vm-visible --win7x86 --iso-mount /mnt/windows_ISOs/ seven0
-
 vmcloak install seven0 adobe9 wic pillow dotnet40 java7
 
-#vmcloak install seven0 office2007 \
-#    office2007.isopath=/path/to/a.iso \
-#    office2007.serialkey=ABC-DEF
+echo
+read -p "Would you like to install Office 2007? Y/N" -n 1 -r
 
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  echo "${YELLOW}What is the path to the iso?${NC}"
+  read path
+  echo "${YELLOW}What is the license key?${NC}"
+  read key
+  vmcloak install seven0 office2007 \
+    office2007.isopath=$path \
+    office2007.serialkey=$key
+  fi
+  
 vmcloak snapshot seven0 cuckoo1 192.168.56.101
 fi
