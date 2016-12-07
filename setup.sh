017fd41ee37e28ad28fdb938f7b1dec9de13ed13
benrau87 <benrau87@users.noreply.github.com>
2016-12-07 14:54:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,42 +11,41 @@ echo -e "${YELLOW}What would you like your Cuckoo username to be?${NC}"
 read name
 adduser $name
 
-#Add startup script to cuckoo users home folder
+##Add startup script to cuckoo users home folder
 chmod +x start_cuckoo.sh
 chown $name:$name start_cuckoo.sh
 mv start_cuckoo.sh /home/$name/
 
-#cp *.conf /home/$name/
+##Start mongodb 
 cp mongodb.service /etc/systemd/system/
-#cp start_cuckoo.sh /home/$home/cuckoo/
+
+##Create directories for later
 cd /home/$name/
 dir=$PWD
 mkdir tools/
 cd tools/
+
 ##Depos add
 echo -e "${RED}Installing Dependencies...Please Wait${NC}"
 #Mongodb
 apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
+##Holding pattern for dpkg...
 echo "Waiting for dpkg process to free up..."
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
    sleep 1
 done
-
 apt-get -qq update -y
 apt-get -qq install wireshark mongodb-org=3.2.11 tcpdump virtualbox python python-pip python-dev libvirt-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev libcap2-bin python-dnspython -y
-#apt-get -qq install python python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev mongodb virtualbox tcpdump wireshark -y
-#apt-get install mongodb libffi-dev build-essential python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet tcpdump wireshark virtualbox -y
-#apt-get -qq install python python-pip python-dev libcap2-bin libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg-dev tcpdump mongodb virtualbox -y
-#apt-get -qq install mongodb libffi-dev build-essential python-django python python-dev python-pip python-pil python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile python-chardet virtualbox tcpdump -y
 apt-get -qq dist-upgrade -y
 pip install --upgrade pip
+
+##Add user to vbox and enable mongodb
 usermod -a -G vboxusers $name
 systemctl start mongodb
 sleep 10
 systemctl enable mongodb
-#systemctl enable mongodb.service
+
 ##tcpdump permissions
 setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 
@@ -94,37 +93,31 @@ git clone https://github.com/jbremer/vmcloak.git
 tar -xvzf v1.0.tar.gz
 
 ##Cuckoo
-#cd $dir
 cd /etc/
-#git clone https://github.com/cuckoosandbox/cuckoo.git
 git clone https://github.com/spender-sandbox/cuckoo-modified.git
-#wget https://downloads.cuckoosandbox.org/2.0-rc2/cuckoo-2.0-rc2.tar.gz
-#tar -xvzf cuckoo-2.0-rc2.tar.gz
 pip install -r cuckoo-modified/requirements.txt
 pip install django-ratelimit
 cd cuckoo-modified/utils/
 python comm* --all --force
-#Copy over conf files
+
+##Copy over conf files
 cd $gitdir/
 cp cuckoo.conf reporting.conf virtualbox.conf /etc/cuckoo-modified/conf/
+##Add vmcloak scripts 
 chmod +x vmcloak.sh
 cp vmcloak.sh $dir/
-#rm *.conf
+##Add windows python and PIL installers for VMs
 cd $dir
 mkdir windows_python_exe/
 cd windows_python_exe/
 wget http://effbot.org/downloads/PIL-1.1.7.win32-py2.7.exe
 wget https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi
 
+##Change ownership for folder that have been created
 chown -R $name:$name /home/$name/*
 chown -R $name:$name /etc/cuckoo-modified/*
 chmod -R 777 /etc/cuckoo-modified/
-#Create mongo database and make cuckoo user owner
-#cd $dir/
-#mkdir /data
-#mkdir /data/db
 
-#chown -R $name:$name /data/*
 ###Setup of VirtualBox forwarding rules and host only adapter
 VBoxManage hostonlyif create
 VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1
@@ -132,6 +125,8 @@ iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstat
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE
 sudo sysctl -w net.ipv4.ip_forward=1
+
+##Below can be enabled if using a external DB connection
 #iptables -A INPUT -s 0.0.0.0 -p tcp --destination-port 27017 -m state --state NEW,ESTABLISHED -j ACCEPT
 #iptables -A OUTPUT -d 0.0.0.0 -p tcp --source-port 27017 -m state --state ESTABLISHED -j ACCEPT
 
@@ -147,5 +142,5 @@ then
 apt-get -qq install iptables-persistent -y
 fi
 echo
-echo -e "${YELLOW}Installation complete, login as $name and open the terminal. In $name home folder you will find the cuckoo client. To get started as fast as possible you will need to create a virtualbox vm and name it ${RED}Win7 Clone${NC}.${YELLOW} Take a snapshot after it has been created and is running the agent and python 27. You can then launch cuckoo_start.sh and navigate to $HOSTNAME:8000${NC}"
+echo -e "${YELLOW}Installation complete, login as $name and open the terminal. In $name home folder you will find the start_cuckoo script. To get started as fast as possible you will need to create a virtualbox vm and name it ${RED}cuckoo1${NC}.${YELLOW} Take a snapshot after it has been created and is running the agent and python 27. Name the snapshot ${RED}vmcloak${YELLOW}. Alternatively you can create the VM with the vmcloak.sh script provided in your home directory. This will require you have a local copy of the Windows ISO you wish to use. You can then launch cuckoo_start.sh and navigate to $HOSTNAME:8000${NC}"
 
