9749bc3046cc1d81d48ef773795ac5609cdc1f1c
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 11:36:09 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -119,8 +119,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo
 print_status "${YELLOW}Installing Dependencies...Please Wait${NC}"
 #Mongodb
-apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 ##Suricata
 add-apt-repository ppa:oisf/suricata-beta -y
 ##Holding pattern for dpkg...
