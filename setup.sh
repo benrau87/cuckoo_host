f5375742b7889cec0524d5bd28c28efe604ed59d
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 09:35:30 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ cd tools/
 apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 ##Suricata
-add-apt-repository ppa:oisf/suricata-beta
+add-apt-repository ppa:oisf/suricata-beta -y
 ##Holding pattern for dpkg...
 echo -e "${YELLOW}Waiting for dpkg process to free up...If this takes too long try running ${RED}sudo rm -f /var/lib/dpkg/lock${YELLOW} in another terminal window.${NC}"
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
