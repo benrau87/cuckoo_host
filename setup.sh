7a0a2fe55cc570e1c77b6a147ba831c3f9375c48
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 13:01:49 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -285,24 +285,28 @@ echo
 read -p "Do you want to iptable changes persistent? Y/N" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+echo
 apt-get -qq install iptables-persistent -y
 fi
 echo
 read -p "Would you like to create VMs at this time? Y/N" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+echo
 bash $dir/vmcloak.sh
 fi
 echo
 read -p "Would you like to harden this host from malware Y/N" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-apt-get install unattended-upgrades apt-listchanges fail2ban -y
+echo
+apt-get install -qq unattended-upgrades apt-listchanges fail2ban -y
 fi
 echo
 read -p "Would you like secure the Cuckoo webserver with SSL? Y/N" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+echo
 bash $gitdir/nginx.sh
 fi
 echo -e "${YELLOW}Installation complete, login as $name and open the terminal. In $name home folder you will find the start_cuckoo script. To get started as fast as possible you will need to create a virtualbox vm and name it ${RED}cuckoo1${NC}.${YELLOW} Take a snapshot after it has been created and is running the agent and python 27. Name the snapshot ${RED}vmcloak${YELLOW}. Alternatively you can create the VM with the vmcloak.sh script provided in your home directory. This will require you have a local copy of the Windows ISO you wish to use. You can then launch cuckoo_start.sh and navigate to $HOSTNAME:8000 or https://$HOSTNAME if Nginx was installed.${NC}"
