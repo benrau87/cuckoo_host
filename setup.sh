c2d3f5ffa6832701fa3f5ca59a359b18629115cf
benrau87 <benrau87@users.noreply.github.com>
2016-12-29 12:02:30 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -118,8 +118,10 @@ print_status "${YELLOW}Adding Repositories...Please Wait${NC}"
 #Mongodb
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
 #echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
+#error_check 'Mongodb added'
 ##Suricata
 add-apt-repository ppa:oisf/suricata-beta -y &>> $logfile
+error_check 'Suricata added'
 ##Holding pattern for dpkg...
 print_status "${YELLOW}Waiting for dpkg process to free up...${NC}"
 print_status "${YELLOW}If this takes too long try running ${RED}sudo rm -f /var/lib/dpkg/lock${YELLOW} in another terminal window.${NC}"
