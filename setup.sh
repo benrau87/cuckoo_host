b1b24ef32df00b573a26d2dc84dba2abff311f4d
benrau87 <benrau87@users.noreply.github.com>
2017-01-19 12:11:51 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -115,7 +115,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo
 print_status "${YELLOW}Adding Repositories...Please Wait${NC}"
-Mongodb
+
+##Mongodb
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
 error_check 'Mongodb added'
